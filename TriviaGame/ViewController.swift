76e9b28ff997ca0698a0818712753e197f6760d2
Johan <johanlindstroom@icloud.com>
2020-01-17 08:50:55 +0100

    Initial Commit

---
+++ TriviaGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TriviaGame
+//
+//  Created by Johan Lindström on 2020-01-17.
+//  Copyright © 2020 Johan Lindström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
